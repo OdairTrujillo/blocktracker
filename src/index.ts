8b42dd6a187acf17fa98d51e10e0732bc2349b4f
JesusTorresML <jesust07@gmail.com>
2024-05-11 16:37:20 -0500

    Added data in usd calcualtion

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export { tradesHistory } from './tracker.js';
-
+import { Protocol } from 'lib';
 import { Config, BackendSelectorByChain } from 'lib';
-import { BackendSelector } from 'lib';
+import { BackendSelector, logger } from 'lib';
+import { ReadOnlyOracle, ReadOnlyOraclesByChain } from 'oracle';
 
 import { trackTrades } from './tracker.js';
 
@@ -9,6 +10,26 @@ import './mods.js'; // Polyfill
 
 export const fullBackendSelectors: BackendSelectorByChain = {} as BackendSelectorByChain;
 
+export const oracles: ReadOnlyOraclesByChain = {} as ReadOnlyOraclesByChain;
+
+// Instantiation of oracles from config.json
+logger.info(`Initializing oracles ...`, { module: 'Blocktracker' });
+for (const blockchain of Config.blockchains) {
+  const protocols: Array<Protocol> = Config.protocols.filter(
+    (protocol: Protocol) => protocol.chain === blockchain.name
+  );
+  // Preparing oracles promises.
+  const oraclePromises: Array<Promise<ReadOnlyOracle>> = protocols.map(
+    async (protocol: Protocol) => {
+      const pairs: object = { startIndex: 0 };
+      const oracle: ReadOnlyOracle = new ReadOnlyOracle(blockchain, protocol);
+      await oracle.init(pairs);
+      return oracle;
+    }
+  );
+  // Resolve oracles promises for each chain.
+  oracles[blockchain.name] = await Promise.all(oraclePromises);
+}
 // Filling backends selectors.
 for (const blockchain of Config.blockchains) {
   fullBackendSelectors[blockchain.name] = BackendSelector('fullNode', blockchain.name);
--- src/tracker.ts
+++ src/tracker.ts
@@ -1,11 +1,15 @@
 import { EthersError } from 'ethers';
 
-import { CustomRpcProvider, BackendSelector } from 'lib';
+import { CustomRpcProvider, BackendSelector, TradeData, ProtocolCode } from 'lib';
 import { Config, logger, saveObject, readObject, Protocol } from 'lib';
-import { Trade, TradesByChain } from 'lib';
+import { Trade, TradesByChain, Price } from 'lib';
+import { Address, PairElement, absBigInt } from 'lib';
+
+import { ReadOnlyOracle } from 'oracle';
+import { toUnsafeFloat, toPrice, getChainCoinPrice } from 'oracle';
 
 import { getBlockTrades } from './transactions.js';
-
+import { oracles } from './index.js';
 // This array will store 24 hours of traded pairs.
 export const tradesHistory: TradesByChain = {} as TradesByChain;
 
@@ -49,7 +53,7 @@ export function trackTrades(): void {
 
     // To store trades for each blokchain.
     let tradesByInterval: Array<Trade> = [];
-
+    const tradesFinalByInterval: Array<TradeData> = [];
     // Register event for new blocks for each unique blockchain provider.
     provider.on('block', async (blockNumber: number) => {
       if (blockNumber % 3 === 0) {
@@ -59,9 +63,138 @@ export function trackTrades(): void {
             blockNumber - 1,
             { backendSelector: backendSelector }
           );
-
+          const chainCoinPrice: number = getChainCoinPrice(blockchain);
           // Process pairs if there were protocols traded.
           if (blockTrades !== null) {
+            const oraclesForThisChain: Array<ReadOnlyOracle> = oracles[blockchain.name];
+            const pairElements: Array<PairElement> = [];
+            for (const oracle of oraclesForThisChain) {
+              const pairAddresses: Array<Address> = blockTrades.map(
+                (trade: Trade) => trade.pairAddress
+              );
+              pairElements.push(
+                ...oracle.getPairElements(
+                  { pairAddresses: pairAddresses },
+                  { enabled: true }
+                )
+              );
+            }
+
+            // AllPairAddress viene de los pairElement del oráculo pero no necsariamente son todos los pairAddress que vienen del bloque
+            const allPairAddrs: Array<Address> = pairElements.map(
+              (item: PairElement) => item.pairAddress
+            );
+            const blockTradesFiltered: Array<Trade> = blockTrades.filter((trade: Trade) =>
+              allPairAddrs.includes(trade.pairAddress)
+            );
+            const sortedPairElements: Array<PairElement> = pairElements.sort((a, b) => {
+              if (a.pairAddress === null && b.pairAddress !== null) {
+                return 1;
+              }
+              if (a.pairAddress !== null && b.pairAddress === null) {
+                return -1;
+              }
+              if (a.pairAddress === null && b.pairAddress === null) {
+                return 0;
+              }
+              if (a.pairAddress < b.pairAddress) {
+                return -1;
+              } else if (a.pairAddress > b.pairAddress) {
+                return 1;
+              } else {
+                return 0;
+              }
+            });
+            const blockTradesSorted: Array<Trade> = blockTradesFiltered.sort((a, b) => {
+              if (a.pairAddress === null && b.pairAddress !== null) {
+                return 1;
+              }
+              if (a.pairAddress !== null && b.pairAddress === null) {
+                return -1;
+              }
+              if (a.pairAddress === null && b.pairAddress === null) {
+                return 0;
+              }
+              // Ambos tienen pairAddress no nulo, entonces comparamos normalmente
+              if (a.pairAddress < b.pairAddress) {
+                return -1;
+              } else if (a.pairAddress > b.pairAddress) {
+                return 1;
+              } else {
+                return 0;
+              }
+            });
+
+            let index: number = 0;
+            for (const pairElement of sortedPairElements) {
+              const trade: Trade = blockTradesSorted[index];
+              const token0Address: Address = pairElement.token0Address as Address;
+              const token1Address: Address = pairElement.token1Address as Address;
+              const token0Symbol: string = pairElement.token0Symbol as string;
+              const token1Symbol: string = pairElement.token1Symbol as string;
+              const amount0: bigint = trade.amount0
+                ? trade.amount0
+                : absBigInt((trade.amount0Out ?? 0n) - (trade.amount0In ?? 0n));
+
+              const amount1: bigint = trade.amount1
+                ? trade.amount1
+                : absBigInt((trade.amount1Out ?? 0n) - (trade.amount1In ?? 0n));
+
+              const protocolCode: ProtocolCode = trade.protocolCode;
+              const amount0Tokens: number = toUnsafeFloat(
+                amount0,
+                Number(pairElement.token0Decimals)
+              );
+              const amount1Tokens: number = toUnsafeFloat(
+                amount1,
+                Number(pairElement.token1Decimals)
+              );
+
+              let relPairElement: PairElement | undefined = undefined;
+
+              const oracle: ReadOnlyOracle | undefined = oraclesForThisChain.find(
+                (oracle: ReadOnlyOracle) => oracle.protocol.code === protocolCode
+              );
+              // relPairElement will no overrided if oracle is undefined.
+              if (oracle !== undefined && pairElement.meta.normal === false) {
+                // Get the related pair for any of both tokens in case of abc|xyz pairs.
+                relPairElement = oracle.getRelPairElement(token0Address, token1Address);
+              }
+
+              const price: Price = await toPrice(
+                blockchain,
+                protocolCode,
+                pairElement,
+                amount0,
+                amount1,
+                chainCoinPrice,
+                { relPairElement: relPairElement, backendSelector: backendSelector }
+              );
+
+              const tradedAmountFloat: number =
+                price.side === 'price0' ? amount0Tokens : amount1Tokens;
+
+              const tradeDataUsd: TradeData = {
+                protocolCode: trade.protocolCode,
+                pairAddress: trade.pairAddress,
+                txHash: trade.txHash,
+                router: trade.router,
+                trader: trade.trader,
+                recipient: trade.recipient,
+                tokenA: price.side === 'price0' ? token0Address : token1Address,
+                tokenB: price.side === 'price0' ? token1Address : token0Address,
+                tokenASymbol: price.side === 'price0' ? token0Symbol : token1Symbol,
+                tokenBSymbol: price.side === 'price0' ? token1Symbol : token0Symbol,
+                timestamp: trade.timestamp,
+                amountA: price.side === 'price0' ? amount0Tokens : amount1Tokens,
+                amountB: price.side === 'price0' ? amount1Tokens : amount0Tokens,
+                priceUsd: price.value,
+                tradedAmountUsd: tradedAmountFloat * price.value
+              };
+              tradesFinalByInterval.push(tradeDataUsd);
+              index++;
+            }
+
             tradesByInterval.push(...blockTrades);
           } else {
             logger.warn(
@@ -87,7 +220,7 @@ export function trackTrades(): void {
     const historyInterval: NodeJS.Timeout = setInterval(async () => {
       try {
         // Add one interval of data.
-        tradesHistory[blockchain.name].push(structuredClone(tradesByInterval));
+        tradesHistory[blockchain.name].push(structuredClone(tradesFinalByInterval));
 
         // Removing the elder element of history pool
         if (tradesHistory[blockchain.name].length > blockchain.cacheCapacity) {
