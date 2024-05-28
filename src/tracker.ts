5b377f80ad976096947b109248ff2788c58dac24
OdairTrujillo <haldrik@gmail.com>
2024-05-28 17:17:01 -0500

    Fallback to TradesyyyyMMdd format for collections

--- src/tracker.ts
+++ src/tracker.ts
@@ -255,7 +255,7 @@ export function trackTrades(): void {
     // Store pairs that were fetched each time interval.
     const storeTradesInterval: NodeJS.Timeout = setInterval(async () => {
       const currentDate: Date = new Date();
-      const tradesCollectionName: string = format(currentDate, 'yyyyMMdd') + 'Trades';
+      const tradesCollectionName: string = 'Trades' + format(currentDate, 'yyyyMMdd');
 
       try {
         const result: Trade | null = await addTrades(
